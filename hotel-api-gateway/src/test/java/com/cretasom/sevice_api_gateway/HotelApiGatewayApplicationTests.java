32c10dccf525abb2947a405810c76c27afd1f406
crestasom <41513543+shrest0000@users.noreply.github.com>
2024-07-10 07:26:52 +0545

    added service discovery and api gateway

---
+++ hotel-api-gateway/src/test/java/com/cretasom/sevice_api_gateway/HotelApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cretasom.sevice_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
